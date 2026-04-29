a1bace37e9e4b1048c66bfa8ad53b4d2af395ac1
Chanelle Mosquera <25163297+chmosquera@users.noreply.github.com>
2026-04-28 21:15:17 -0700

    Set up two frames for rendering, one to show and a second to prepare for the next frame. A command buffer is associated with a queue of a particular type (e.g. graphics). When working with queues and command buffers, we also need to specify the 'queue family'. A family is a grouping of queues that share the same capabilities. Queues of the same family typically come from the same GPU/driver.

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,6 +5,13 @@
 
 #include <vk_types.h>
 
+struct FrameData {
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine {
 public:
 
@@ -14,7 +21,7 @@ public:
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
-
+	
 	static VulkanEngine& Get();
 
 	//initializes everything in the engine
@@ -42,6 +49,12 @@ public:
 	std::vector<VkImageView>_swapchainImageViews;
 	VkExtent2D _swapchainExtent;
 
+	FrameData _frames[FRAME_OVERLAP];
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
 
 private:
 
@@ -52,5 +65,7 @@ private:
 
 	void create_swapchain(uint32_t width, uint32_t height);
 	void destroy_swapchain();
+
+
 
 };
