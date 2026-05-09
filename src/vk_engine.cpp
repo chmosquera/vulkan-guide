4a90e6995140562e8f7c4663643ddedbe0ae9d5e
chmosquera <25163297+chmosquera@users.noreply.github.com>
2026-05-09 07:51:18 -0700

    Created deletion queue and allocate images using AMD Vulkan Memory Allocator; Minor clang-formatting. > > Deletion queue is done in LIFO order. As objects are created, we add them to the deletion queue. Current implementation is to queue functions, which isn't efficient at scale, but suited for this project for now. A better implementation would be to store handles.
    Define VMA_IMPLEMENTATION stores the definitions of VMA functions; the functions themselves are implemented in the vk_mem_alloc.h header.
    -

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,17 +1,19 @@
 ﻿//> includes
 #include "vk_engine.h"
 #include "vk_images.h"
+#include <vk_initializers.h>
+#include <vk_types.h>
 
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
-#include <vk_initializers.h>
-#include <vk_types.h>
-
 #include <VkBootstrap.h>
 
-#include <iostream>
+#define VMA_IMPLEMENTATION // one definition for the whole project
+#include "vk_mem_alloc.h"
+
 #include <chrono>
+#include <iostream>
 #include <thread>
 
 VulkanEngine* loadedEngine = nullptr;
@@ -28,6 +30,7 @@ void VulkanEngine::init()
 
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
 
+    // clang-format off
     _window = SDL_CreateWindow(
         "Vulkan Engine",
         SDL_WINDOWPOS_UNDEFINED,
@@ -36,6 +39,7 @@ void VulkanEngine::init()
         _windowExtent.height,
         window_flags
     );
+    // clang-format on
 
     init_vulkan();
 
@@ -51,19 +55,28 @@ void VulkanEngine::init()
 
 void VulkanEngine::cleanup()
 {
-    if (_isInitialized) {
-
+    if (_isInitialized)
+    {
         vkDeviceWaitIdle(_device);
 
-        for (int i = 0; i < FRAME_OVERLAP; i++) {
+        for (int i = 0; i < FRAME_OVERLAP; i++)
+        {
             vkDestroyCommandPool(_device, _frames[i]._commandPool, nullptr);
+
+            // destroy sync objects
+            vkDestroyFence(_device, _frames[i]._renderFence, nullptr);
+            vkDestroySemaphore(_device, _frames[i]._renderSemaphore, nullptr);
+            vkDestroySemaphore(_device, _frames[i]._swapchainSemaphore, nullptr);
+
+            _frames[i]._deletionQueue.flush();
         }
 
         vkDestroyInstance(_instance, nullptr);
 
         vkDestroySwapchainKHR(_device, _swapchain, nullptr);
 
-        for (int i = 0; i < _swapchainImageViews.size(); i++) {
+        for (int i = 0; i < _swapchainImageViews.size(); i++)
+        {
             vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
         }
 
@@ -76,7 +89,8 @@ void VulkanEngine::cleanup()
     // clear engine pointer
     loadedEngine = nullptr;
 
-    for (int i = 0; i < FRAME_OVERLAP; i++) {
+    for (int i = 0; i < FRAME_OVERLAP; i++)
+    {
         vkDestroyCommandPool(_device, _frames[i]._commandPool, nullptr);
 
         // destroy sync objects
@@ -94,8 +108,11 @@ void VulkanEngine::draw()
     VK_CHECK(vkWaitForFences(_device, 1, &get_current_frame()._renderFence, true, 1000000000));
     VK_CHECK(vkResetFences(_device, 1, &get_current_frame()._renderFence));
 
+    get_current_frame()._deletionQueue;
+
     uint32_t swapchainImageIndex;
-    VK_CHECK(vkAcquireNextImageKHR(_device, _swapchain, 1000000000, get_current_frame()._swapchainSemaphore, nullptr, &swapchainImageIndex));
+    VK_CHECK(vkAcquireNextImageKHR(
+        _device, _swapchain, 1000000000, get_current_frame()._swapchainSemaphore, nullptr, &swapchainImageIndex));
 
     VkCommandBuffer cmd = get_current_frame()._mainCommandBuffer;
 
@@ -105,12 +122,14 @@ void VulkanEngine::draw()
 
     // VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT - Minor optimization
     // Tells GPU we use buffer only once, so don't need to store anything
-    VkCommandBufferBeginInfo cmdBeginInfo = vkinit::command_buffer_begin_info(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+    VkCommandBufferBeginInfo cmdBeginInfo =
+        vkinit::command_buffer_begin_info(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
 
     VK_CHECK(vkBeginCommandBuffer(cmd, &cmdBeginInfo));
 
     // Transition image into a writeable mode
-    vkutil::transition_image(cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL);
+    vkutil::transition_image(
+        cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL);
 
     VkClearColorValue clearValue;
     float flash = std::abs(std::sin(_frameNumber / 120.f));
@@ -118,15 +137,19 @@ void VulkanEngine::draw()
 
     VkImageSubresourceRange clearRange = vkinit::image_subresource_range(VK_IMAGE_ASPECT_COLOR_BIT);
 
-    vkCmdClearColorImage(cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange);
-    vkutil::transition_image(cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_GENERAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+    vkCmdClearColorImage(
+        cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange);
+    vkutil::transition_image(
+        cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_GENERAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
 
     VK_CHECK(vkEndCommandBuffer(cmd));
 
     // Submission
     VkCommandBufferSubmitInfo cmdInfo = vkinit::command_buffer_submit_info(cmd);
-    VkSemaphoreSubmitInfo waitInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT_KHR, get_current_frame()._swapchainSemaphore);
-    VkSemaphoreSubmitInfo signalInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_ALL_GRAPHICS_BIT, get_current_frame()._renderSemaphore);
+    VkSemaphoreSubmitInfo waitInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT_KHR,
+                                                                   get_current_frame()._swapchainSemaphore);
+    VkSemaphoreSubmitInfo signalInfo =
+        vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_ALL_GRAPHICS_BIT, get_current_frame()._renderSemaphore);
 
     VkSubmitInfo2 submit = vkinit::submit_info(&cmdInfo, &signalInfo, &waitInfo);
 
@@ -155,25 +178,31 @@ void VulkanEngine::run()
     bool bQuit = false;
 
     // main loop
-    while (!bQuit) {
+    while (!bQuit)
+    {
         // Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
+        while (SDL_PollEvent(&e) != 0)
+        {
             // close the window when user alt-f4s or clicks the X button
             if (e.type == SDL_QUIT)
                 bQuit = true;
 
-            if (e.type == SDL_WINDOWEVENT) {
-                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+            if (e.type == SDL_WINDOWEVENT)
+            {
+                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED)
+                {
                     stop_rendering = true;
                 }
-                if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+                if (e.window.event == SDL_WINDOWEVENT_RESTORED)
+                {
                     stop_rendering = false;
                 }
             }
         }
 
         // do not draw if we are minimized
-        if (stop_rendering) {
+        if (stop_rendering)
+        {
             // throttle the speed to avoid the endless spinning
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             continue;
@@ -185,75 +214,87 @@ void VulkanEngine::run()
 
 constexpr bool bUseValidationLayers = true;
 
-void VulkanEngine::init_vulkan() {
-
+void VulkanEngine::init_vulkan()
+{
     vkb::InstanceBuilder builder;
 
     auto inst_ret = builder.set_app_name("Example Vulkan Application")
-        .request_validation_layers(bUseValidationLayers)
-        .use_default_debug_messenger()
-        .require_api_version(1, 3, 0)
-        .build();
+                        .request_validation_layers(bUseValidationLayers)
+                        .use_default_debug_messenger()
+                        .require_api_version(1, 3, 0)
+                        .build();
 
-        vkb::Instance vkb_inst = inst_ret.value();
-        
-        _instance = vkb_inst.instance;
-        _debug_messenger = vkb_inst.debug_messenger;
+    vkb::Instance vkb_inst = inst_ret.value();
 
-        // Create surface from SDL window
-        SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+    _instance = vkb_inst.instance;
+    _debug_messenger = vkb_inst.debug_messenger;
 
-        VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES};
-        features.dynamicRendering = true;
-        features.synchronization2 = true;
+    // Create surface from SDL window
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
 
-        VkPhysicalDeviceVulkan12Features features12 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES};
-        features12.bufferDeviceAddress = true;
-        features12.descriptorIndexing = true;
+    VkPhysicalDeviceVulkan13Features features{.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES};
+    features.dynamicRendering = true;
+    features.synchronization2 = true;
 
-        // By telling vkb to selector what version features we will use,
-        // vkb will find a gpu that supports those
-        vkb::PhysicalDeviceSelector selector { vkb_inst };
-        vkb::PhysicalDevice physicalDevice = selector
-            .set_minimum_version(1,3)
-            .set_required_features_13(features)
-            .set_required_features_12(features12)
-            .set_surface(_surface)
-            .select()
-            .value();
+    VkPhysicalDeviceVulkan12Features features12{.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES};
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
 
-        vkb::DeviceBuilder deviceBuilder{ physicalDevice };
-        
-        vkb::Device vkbDevice = deviceBuilder.build().value();
+    // By telling vkb to selector what version features we will use,
+    // vkb will find a gpu that supports those
+    vkb::PhysicalDeviceSelector selector{vkb_inst};
+    vkb::PhysicalDevice physicalDevice = selector.set_minimum_version(1, 3)
+                                             .set_required_features_13(features)
+                                             .set_required_features_12(features12)
+                                             .set_surface(_surface)
+                                             .select()
+                                             .value();
 
-        _device = vkbDevice.device;
-        _chosenGPU = physicalDevice.physical_device;
+    vkb::DeviceBuilder deviceBuilder{physicalDevice};
 
-        _graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
-        _graphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    _device = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
+
+    _graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+    _graphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+
+    // init the memory allocator
+    VmaAllocatorCreateInfo allocatorInfo = {};
+    allocatorInfo.physicalDevice = _chosenGPU;
+    allocatorInfo.device = _device;
+    allocatorInfo.instance = _instance;
+    allocatorInfo.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+    vmaCreateAllocator(&allocatorInfo, &_allocator);
+
+    _mainDeletionQueue.push_function([&]()
+    {
+        vmaDestroyAllocator(_allocator);
+    });
 }
 
 
-
-void VulkanEngine::init_swapchain() {
-    create_swapchain(_windowExtent.width, _windowExtent.height);
-}
+void VulkanEngine::init_swapchain() { create_swapchain(_windowExtent.width, _windowExtent.height); }
 
 
-void VulkanEngine::init_commands() {
-    VkCommandPoolCreateInfo commandPoolInfo = vkinit::command_pool_create_info(_graphicsQueueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+void VulkanEngine::init_commands()
+{
+    VkCommandPoolCreateInfo commandPoolInfo =
+        vkinit::command_pool_create_info(_graphicsQueueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
 
-    for (int i = 0; i < FRAME_OVERLAP; i++) {
+    for (int i = 0; i < FRAME_OVERLAP; i++)
+    {
         VK_CHECK(vkCreateCommandPool(_device, &commandPoolInfo, nullptr, &_frames[i]._commandPool));
 
         VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::command_buffer_allocate_info(_frames[i]._commandPool, 1);
 
         VK_CHECK(vkAllocateCommandBuffers(_device, &cmdAllocInfo, &_frames[i]._mainCommandBuffer));
     }
-
 }
 
-void VulkanEngine::init_sync_structures() {
+void VulkanEngine::init_sync_structures()
+{
 
     // Initialize the fence as signaled, so the first frame waits on it
     // Without this, if the GPU is busy at start, then the thread gets blocked
@@ -261,7 +302,8 @@ void VulkanEngine::init_sync_structures() {
 
     VkSemaphoreCreateInfo semaphoreCreateInfo = vkinit::semaphore_create_info();
 
-    for (int i = 0; i < FRAME_OVERLAP; i++) {
+    for (int i = 0; i < FRAME_OVERLAP; i++)
+    {
         VK_CHECK(vkCreateFence(_device, &fenceCreateInfo, nullptr, &_frames[i]._renderFence));
 
         VK_CHECK(vkCreateSemaphore(_device, &semaphoreCreateInfo, nullptr, &_frames[i]._swapchainSemaphore));
@@ -269,31 +311,35 @@ void VulkanEngine::init_sync_structures() {
     }
 }
 
-void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
-    vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU, _device, _surface };
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+    vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, _device, _surface};
 
     _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
 
-    vkb::Swapchain vkbSwapchain = swapchainBuilder
-        // .use_default_format_selection()
-        .set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
-        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
-        .set_desired_extent(width, height)
-        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
-        .build()
-        .value();
+    vkb::Swapchain vkbSwapchain =
+        swapchainBuilder
+            // .use_default_format_selection()
+            .set_desired_format(
+                VkSurfaceFormatKHR{.format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR})
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+            .set_desired_extent(width, height)
+            .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+            .build()
+            .value();
 
     _swapchainExtent = vkbSwapchain.extent;
     _swapchain = vkbSwapchain.swapchain;
     _swapchainImages = vkbSwapchain.get_images().value();
     _swapchainImageViews = vkbSwapchain.get_image_views().value();
-
 }
 
-void VulkanEngine::destroy_swapchain() {
+void VulkanEngine::destroy_swapchain()
+{
     vkDestroySwapchainKHR(_device, _swapchain, nullptr);
 
-    for (int i = 0; i < _swapchainImageViews.size(); i++) {
+    for (int i = 0; i < _swapchainImageViews.size(); i++)
+    {
         vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
     }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,69 +5,88 @@
 
 #include <vk_types.h>
 
-struct FrameData {
-	VkCommandPool _commandPool;
-	VkCommandBuffer _mainCommandBuffer;
-	VkSemaphore _swapchainSemaphore, _renderSemaphore;
-	VkFence _renderFence;
-};
-
 constexpr unsigned int FRAME_OVERLAP = 2;
 
-class VulkanEngine {
+struct DeletionQueue
+{
+    std::deque<std::function<void()>> deletors;
+
+    void push_function(std::function<void()>&& function) { deletors.push_back(function); }
+
+    void flush()
+    {
+        // Call the deletors in reverse order - LIFOc
+        for (auto it = deletors.rbegin(); it != deletors.rend(); ++it)
+        {
+            (*it)();
+        }
+
+        deletors.clear();
+    }
+};
+
+struct FrameData
+{
+    VkCommandPool _commandPool;
+    VkCommandBuffer _mainCommandBuffer;
+    VkSemaphore _swapchainSemaphore, _renderSemaphore;
+    VkFence _renderFence;
+    DeletionQueue _deletionQueue;
+};
+
+class VulkanEngine
+{
 public:
+    bool _isInitialized{false};
+    int _frameNumber{0};
+    bool stop_rendering{false};
+    VkExtent2D _windowExtent{1700, 900};
 
-	bool _isInitialized{ false };
-	int _frameNumber {0};
-	bool stop_rendering{ false };
-	VkExtent2D _windowExtent{ 1700 , 900 };
+    struct SDL_Window* _window{nullptr};
 
-	struct SDL_Window* _window{ nullptr };
-	
-	static VulkanEngine& Get();
+    static VulkanEngine& Get();
 
-	//initializes everything in the engine
-	void init();
+    // initializes everything in the engine
+    void init();
 
-	//shuts down the engine
-	void cleanup();
+    // shuts down the engine
+    void cleanup();
 
-	//draw loop
-	void draw();
+    // draw loop
+    void draw();
 
-	//run main loop
-	void run();
+    // run main loop
+    void run();
 
-	VkInstance _instance;
-	VkDebugUtilsMessengerEXT _debug_messenger;
-	VkPhysicalDevice _chosenGPU; 
-	VkDevice _device;
-	VkSurfaceKHR _surface;
+    VkInstance _instance;
+    VkDebugUtilsMessengerEXT _debug_messenger;
+    VkPhysicalDevice _chosenGPU;
+    VkDevice _device;
+    VkSurfaceKHR _surface;
 
-	VkSwapchainKHR _swapchain;
-	VkFormat _swapchainImageFormat;
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
 
-	std::vector<VkImage> _swapchainImages;
-	std::vector<VkImageView>_swapchainImageViews;
-	VkExtent2D _swapchainExtent;
+    std::vector<VkImage> _swapchainImages;
+    std::vector<VkImageView> _swapchainImageViews;
+    VkExtent2D _swapchainExtent;
 
-	FrameData _frames[FRAME_OVERLAP];
-	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+    FrameData _frames[FRAME_OVERLAP];
+    FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
 
-	VkQueue _graphicsQueue;
-	uint32_t _graphicsQueueFamily;
+    VkQueue _graphicsQueue;
+    uint32_t _graphicsQueueFamily;
 
+    DeletionQueue _mainDeletionQueue;
+
+    VmaAllocator _allocator;
 
 private:
+    void init_vulkan();
+    void init_swapchain();
+    void init_commands();
+    void init_sync_structures();
 
-	void init_vulkan();
- 	void init_swapchain();
-	void init_commands();
-	void init_sync_structures();
-
-	void create_swapchain(uint32_t width, uint32_t height);
-	void destroy_swapchain();
-
-
-
+    void create_swapchain(uint32_t width, uint32_t height);
+    void destroy_swapchain();
 };
